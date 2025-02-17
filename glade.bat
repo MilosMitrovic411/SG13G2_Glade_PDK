7720af43969e5c318759b8d97801662bb33b7b9d
Miloš Mitrović <milos.m.mitrovic98@gmail.com>
2025-02-17 19:41:46 +0100

    DRC auto loading added

--- glade.bat
+++ glade.bat
@@ -1,9 +1,10 @@
 set GLADE_HOME=
 set PATH=%GLADE_HOME%;%PATH%
 set PYTHONHOME=%GLADE_HOME%\Python38
-set PYTHONPATH=.;.\PCells
+set PYTHONPATH=.;.\PCells;.\Verification
 set GLADE_LOGFILE_DIR=.
 set GLADE_DRC_WORK_DIR=.
+set GLADE_DRC_FILE=.\Verification\drc.py
 set GLADE_FASTCAP_WORK_DIR=.
 rem set GLADE_NO_CHECK_VERSION=1
 rem set GLADE_NO_DELETE_TMPFILES=1

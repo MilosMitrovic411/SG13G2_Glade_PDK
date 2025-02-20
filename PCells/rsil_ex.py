fb8332e0f026aa8be43abaf095b3b0d95091abc8
Miloš Mitrović <milos.m.mitrovic98@gmail.com>
2025-02-20 21:45:23 +0100

    Add files via upload

---
+++ PCells/rsil_ex.py
@@ -0,0 +1,44 @@
+#
+# Import the db wrappers
+#
+from ui import *
+from math import *
+#
+# The entry point. The function name *must* match the filename.
+#
+def rsil_ex(cv, ptlist=[[0,0],[1000,0],[1000,1000],[0,1000]]) :
+    lib = cv.lib()
+    dbu = lib.dbuPerUU()
+    npts = len(ptlist)
+    #
+    # Create the recognition region shape
+    #
+    xpts = intarray(npts)
+    ypts = intarray(npts)
+    for i in range (npts) :
+        xpts[i] = ptlist[i][0]
+        ypts[i] = ptlist[i][1]
+    cv.dbCreatePolygon(xpts, ypts, npts, TECH_Y0_LAYER);
+    #
+    # Create pins
+    #
+    plus_net = cv.dbCreateNet("A")
+    cv.dbCreatePin("A", plus_net, DB_PIN_INPUT)
+    minus_net = cv.dbCreateNet("B")
+    cv.dbCreatePin("B", minus_net, DB_PIN_INPUT)
+    #
+    # Set the device modelName property for netlisting
+    #
+    cv.dbAddProp("modelName", "rsil")
+    #
+    # Set the netlisting property
+    #
+    cv.dbAddProp("NLPDeviceFormat", "[@instName] [|A:%] [|B:%] [@modelName] [@w:w=%u] [@l:l=%u]")
+    cv.dbAddProp("NLPDeviceFormatCDL", "x[@instName] [|A:%] [|B:%] [@modelName] [@w:w=%u] [@l:l=%u]")
+    #
+    # Device type
+    cv.dbAddProp("type", "res")
+    #
+    # Commit to db the bounding box
+    #
+    cv.update()
